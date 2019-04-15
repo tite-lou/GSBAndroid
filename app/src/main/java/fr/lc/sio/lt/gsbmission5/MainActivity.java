33588567d050c05de1dcdd387a3d5e22653cf191
baudin-thiefaine <thiefaine.baudinsarlet@gmail.com>
2019-04-15 14:36:13 +0200

    Fin de résolution du problème avec le POST. Tests à venir

--- app/src/main/java/fr/lc/sio/lt/gsbmission5/MainActivity.java
+++ app/src/main/java/fr/lc/sio/lt/gsbmission5/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         mdp = (EditText) findViewById(R.id.edt_mdp);
         SharedPreferences ps = this.getSharedPreferences("default",0);
         SharedPreferences.Editor edit = ps.edit();
-        edit.putString("ip","http://10.20.128.127/gsbAndroid/web/app.php");
+        edit.putString("ip","http://10.20.128.188/gsbAndroid/web/app.php");
         edit.commit();
     }
     public void seConnecter(View vue){
